59b43f89d84c3a392a7a29de15132d8a97f378b6
TrebsiuqT <167358938+TrebsiuqT@users.noreply.github.com>
2024-05-29 01:15:36 -0300

    Add files via upload

---
+++ Proyectos/Etapa0/0_Quisbert_HolaMundo/0_Quisbert_HolaMundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Quisbert_HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo :DD");
+            Console.ReadKey();
+        }
+    }
+}
