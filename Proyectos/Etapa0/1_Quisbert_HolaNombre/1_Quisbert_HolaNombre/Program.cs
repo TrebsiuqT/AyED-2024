43c193a13f355ec97ad67b1a80fac20a70f23e64
TrebsiuqT <167358938+TrebsiuqT@users.noreply.github.com>
2024-05-29 01:21:05 -0300

    Add files via upload

---
+++ Proyectos/Etapa0/1_Quisbert_HolaNombre/1_Quisbert_HolaNombre/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Quisbert_HolaNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su noombre");
+            String name = Console.ReadLine();
+            
+            Console.WriteLine("Hola, "+name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Proyectos/Etapa0/2_Quisbert_TestVariables/2_Quisbert_TestVariables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Quisbert_TestVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definir V
+            String cadena = "15.557294";
+            int entero = int.Parse(Console.ReadLine());
+            bool booleon = false;
+            float flotante = 15.557294f;
+            double doble = 15.557294;
+            char caracter = '1';
+            //Escribir
+            Console.WriteLine("String: "+cadena);
+            Console.WriteLine("int: "+entero);
+            Console.WriteLine("bool: "+booleon);
+            Console.WriteLine("float: "+flotante);
+            Console.WriteLine("double: "+doble);
+            Console.WriteLine("char: "+caracter);
+            
+            //Detercar tecla
+            Console.ReadKey();
+
+        }
+    }
+}
